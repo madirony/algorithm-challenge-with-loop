f2ae448944536e727d6570ffa392c88c17204560
madirony <48685874+madirony@users.noreply.github.com>
2025-12-01 15:29:33 +0900

    [Day-1] BOJ 1967: 트리의 지름 (개선 전)
    https://www.acmicpc.net/problem/1967

---
+++ madirony/1주차/BOJ_1967.java
@@ -0,0 +1,59 @@
+/*
+  link : https://www.acmicpc.net/problem/1967
+  tier : G4
+  type : tree/graph/dfs
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Main
+{
+	static class Node {
+		int v, w;
+		Node(int v, int w){
+			this.v = v;
+			this.w = w;
+		}
+	}
+	static List<List<Node>> list = new ArrayList<>();
+	static boolean[] visited;
+	public static void main (String[] args) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i <= n; i++) list.add(new ArrayList<>());
+		for(int i = 0; i < n-1; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			list.get(u).add(new Node(v, w));
+			list.get(v).add(new Node(u, w));
+		}
+		
+		int ans = -1;
+		for(int i = 1; i <= n; i++){
+			visited = new boolean[n+1];
+			int res = dfs(i, 0);
+			ans = Math.max(ans, res);
+		}
+		System.out.println(ans);
+	}
+	
+	private static int dfs(int p, int sum){
+	    visited[p] = true;
+	    int best = sum;
+	    boolean hasChild = false;
+	
+	    for (Node tmp : list.get(p)){
+	        if (visited[tmp.v]) continue;
+	        hasChild = true;
+	        int val = dfs(tmp.v, sum + tmp.w);
+	        best = Math.max(best, val);
+	    }
+	
+	    if (!hasChild) return sum;
+	    return best;
+	}
+}
